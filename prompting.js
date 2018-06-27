4479e336e0b8a84d1f52a0f70fb9739ece678e70
smjcrane <smjcrane@gmail.com>
2018-06-27 14:23:30 +0100

    more obvious when you are allowed to install

--- prompting.js
+++ prompting.js
@@ -7,13 +7,13 @@ window.addEventListener('beforeinstallprompt', (event) => {
   installPromptEvent = event;
   console.log("Install prompt found");
   // Update the install UI to notify the user app can be installed
-  document.querySelector('#install-button').disabled = false;
+  document.querySelector('#installation').visibility = "visible";
 });
 
 
 document.querySelector('#install-button').addEventListener('click', () => {
   // Update the install UI to remove the install button
-  document.querySelector('#install-button').disabled = true;
+  document.querySelector('#installation').visibility = "hidden";
   // Show the modal add to home screen dialog
   installPromptEvent.prompt();
   // Wait for the user to respond to the prompt
